4dadab5054284c592eaa92381089429e7b602bc4
PancakeRealization <37256229+PancakeRealization@users.noreply.github.com>
2018-06-10 18:16:59 -0700

    added stats channel

--- commands/statschannel.js
+++ commands/statschannel.js
@@ -1,13 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
+    if(err) console.log(err);
     message.channel.send("Hello.")
     try{
     message.channel.send(`Enabled stats channel.`)
     message.guild.createChannel(`Total Users: ${message.guild.memberCount}`, 'voice')
       }catch(e){
-        console.error(e);
         message.channel.send(`We tried to enable the stats channel.`)
     }
 }
