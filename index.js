e6f3a01fae71e7a46e453aafe44f06a8d9c4abf7
PancakeRealization <37256229+PancakeRealization@users.noreply.github.com>
2018-06-10 18:24:24 -0700

    added stats channel

--- index.js
+++ index.js
@@ -212,16 +212,17 @@ bot.on("message", async message => {
          message.delete().catch();
      }
      if(cmd === `${prefix}statschannel enable`){
+        if(err) console.log(err);
         message.channel.send("Hello.")
         try{
         message.channel.send(`Enabled stats channel.`)
         message.guild.createChannel(`Total Users: ${message.guild.memberCount}`, 'voice')
-        message.delete().catch();
           }catch(e){
-            message.delete().catch();
-            message.channel.send(`Error.`)
-          }
-    }
+            console.log(err);
+            message.channel.send(`We tried to enable the stats channel.`)
+            
+        }
+     }
 });
 
 bot.login(tokenfile.token);
