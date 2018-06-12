e2361e9c24b1fc6cb76172b66544e48d750f46fd
PancakeRealization <37256229+PancakeRealization@users.noreply.github.com>
2018-06-11 19:06:54 -0700

    Added github command.

---
+++ commands/github.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    let githubEmbed = new Discord.RichEmbed()
+    .setTitle("Github")
+    .setURL(`https://github.com/PancakeRealization/volatile-bot`)
+    .setFooter("Github Link")
+    .setTimestamp();
+
+    message.delete().catch();
+    message.channel.send(githubEmbed);
+
+}
+
+module.exports.help = {
+    name: "github"
+}
