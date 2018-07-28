1782c80aa8bba35b810d578eda627f0f3e9b9384
PancakeRealization <37256229+PancakeRealization@users.noreply.github.com>
2018-07-28 00:32:29 -0700

    ye

--- commands/aboutbot.js
+++ commands/aboutbot.js
@@ -10,8 +10,7 @@ module.exports.run = async (bot, message, args) =>{
         .addField("Bot Name", bot.user.username)
         .addField("Created By", `PancakeRealization#9394, and Cliffrising_inputs#7468.`)
         .addField("Created On", bot.user.createdAt)
-        .addField("Servers I am in", `${bot.guilds.size}`)
-        .addField("Want help?", `https://discord.gg/Tr65AJb`)
+        .addField("Servers I am in", `${bot.guilds.size}`)y
         .addField("Bot Commands", `Do !help`)
         .addField("Info", `Make a "members-joined", "reports", "incidents".`)
         .addField("More Info", `Use !statschannel-enable`);
